aa11ef06e5d14f7d43d3ab524617d171cdf8ded9
mohamed mallat <mohamedmallat40@users.noreply.github.com>
2018-11-17 23:45:44 +0100

    server api

--- Dev/Api.js
+++ Dev/Api.js
@@ -1,14 +1,47 @@
-var express = require('express')
-var app = express()
+const express = require('express');
+const app = express();
+const bodyparser = require('body-parser');
+const blockchain =require('./blockchain');
+const Vegawallet=new blockchain();
+const uuid = require('uuid/v1');
 
-app.get('/blockchain', function (req, res) {
+const  nodeID =uuid().split('-').join('');
+
+app.use (bodyparser.json()); 
+app.use (bodyparser.urlencoded({extanded:false}));
+
+
+    app.get('/blockchain', function (req, res) {
+     res.send(Vegawallet);
 });
 
-app.post('/transaction', function(req, res){
-});
-res.send('it works...');
+//creat a new transaction
+    app.post('/transaction', function (req, res){
+        /*console.log(req.body);
+    res.send(`the amount of transactio is ${req.body} Vegawallet`);
+    */
+ const blockindex =  Vegawallet.creatnewtransaction(req.body.amount, req.body.sender, req.body.recipient);
+    res.json({note: `Transacton will be added in block ${blockindex}`});
 
+});
+
+
+    // mine a block
 app.get('/mine', function(req,res){
+        const lastblock = Vegawallet.getlastblock();
+        const previousBlockHash = lastblock['hash'] ;   
+        const currentBlockData={
+            transaction: Vegawallet.pendingtransaction,
+            index: lastblock['index']+1
+        };
+        const nonce = Vegawallet.proofofwork(previousBlockHash, currentBlockData);
+        const blockhash =Vegawallet.hashBlock(previousBlockHash, currentBlockData,nonce); 
+        Vegawallet.creatnewtransaction(20,"00",nodeID);
+        const newblock = Vegawallet.creatnewblock(nonce, previousBlockHash, blockhash);
+        res.json({
+            note: "new block mined successfuly",
+            block: newblock
+        })
 });
 
 app.listen(3000, function(){
