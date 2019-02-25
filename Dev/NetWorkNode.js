3094390c164d3bb665b4cee6447555467206fad3
mohamed mallat <mohamedmallat40@users.noreply.github.com>
2019-02-25 19:14:33 +0100

    sync the network

--- Dev/NetWorkNode.js
+++ Dev/NetWorkNode.js
@@ -6,11 +6,11 @@ const Vegawallet=new blockchain();
 const uuid = require('uuid/v1');
 const nodeID =uuid().split('-').join('');
 const port = process.argv[2];
-const rp = require('request-promise');
+const rp = require('request');
+//var rp = require('http').request;
 
-
-app.use (bodyparser.json()); 
-app.use (bodyparser.urlencoded({extended:false}));
+app.use (bodyparser.json()); //
+app.use (bodyparser.urlencoded({extended:false})); 
 
     app.get('/blockchain', function (req, res) {
      res.send(Vegawallet);
@@ -31,86 +31,86 @@ app.use (bodyparser.urlencoded({extended:false}));
 
 
     // mine a block
-app.get('/mine', function(req,res){
-        const lastblock = Vegawallet.getlastblock();
-        const previousBlockHash = lastblock['hash'] ;   
+    app.get('/mine', function(req,res){
+        const lastBlock = Vegawallet.getlastblock();
+        const previousBlockHash = lastBlock['hash'] ;   
         const currentBlockData={
             transaction: Vegawallet.pendingtransaction,
-            index: lastblock['index']+1
+            index: lastBlock['index']+1
         };
         const nonce = Vegawallet.proofofwork(previousBlockHash, currentBlockData);
         const blockhash =Vegawallet.hashBlock(previousBlockHash, currentBlockData,nonce); 
         Vegawallet.creatnewtransaction(20,"00",nodeID);
-        const newblock = Vegawallet.creatnewblock(nonce, previousBlockHash, blockhash);
+        const newBlock = Vegawallet.creatNewBlock(nonce, previousBlockHash, blockhash);
         res.json({
             note: "new block mined successfuly",
-            block: newblock
+            block: newBlock
         });
     });
 
 
 
 
-//register a node and broadcast itthe network
-app.post('/register-and-broadcast-node', function(req,res){
-    const newNodeUrl =req.body.newNodeUrl;
-    if (vegawallet.networknode.indexOf(newNodeUrl)== -1)Vegawallet.networknode.push(newNodeUrl);
+    //register a node and broadcast ti the network
+    
+    app.post('/register-and-broadcast-node', function(req,res){
+    const newNodeUrl = req.body.newNodeUrl;
 
-        const regnodespromises=[];
-        vegawallet.networknode.array.forEach(networknodeUrl => {
-            //register node and point
-            const requestOption ={
-                uri: networknodeUrl +'/register-node',
-                methode: 'post',
-                body: { newNodeUrl: newNodeUrl},
-                json: true
-                
+       if(Vegawallet.netWorkNodes.indexOf(newNodeUrl) == -1) Vegawallet.netWorkNodes.push(newNodeUrl); // n7otto new nodes fil networknodes arrey
+      Vegawallet.netWorkNodes.forEach(newNodeUrl =>{
+           regNodesPromises=[]; 
+            const requestOptions = {
+
+                uri:        newNodeUrl+'/register-node',
+                method:     'POST',
+                body:       {newNodeUrl: newNodeUrl},
+                json:       true
+
             };
 
-            regnodepromises.push(rq(requestOption));
-
-        }); 
-
-    Promise.all(regnodespromises)
-        .then(data => {
+             regNodesPromises.push(rp(requestOptions));
+        });
+        Promise.all(regNodesPromises)
+        .then (data => {
+            
             const bulkRegisterOption = {
-                uri : newNodeUrl+'/regester-nodes-bulk',
-                methode : post,
-                body: {allnetworknodes:[...vegawallet.networknodes,Vegawallet.currentNodeUrl]},
-                json : true
-        };
-        return rp(bulkRegisterOption);
-    })
-    .then(data =>{
-        res.json({note: 'new node regester with network succefuly'});
-    });
-});
 
-
-
-
-//register a node with the network
-app.post('/register-node', function(req,res){
-    const newNodeUrl = req.body.newNodeUrl;
-    const nodenotalreadypreast = Vegawallet.networknodes.indexOf(newNodeUrl) == -1;
-    const notecurrentnode = Vegawallet.currentNodeUrl !== newNodeUrl;
-    if  (nodenotalreadypreast && notecurrentnode)Vegawallet.networknodes.push(newNodeUrl);
-    res.json({  note : 'new node regestered successfully.  '}); // bech n7outo node jdida fi arrey mta3 nodes
-                                                                 // if bech ntastiw beha ken node déja fil arrey
+                uri:        newNodeUrl + '/register-nodes-bulk',
+                method :    'POST',
+                body:       {  allNetWorkNodes: [   ...Vegawallet.netWorkNodes, Vegawallet.currentNodeUrl] },
+                json:       true
+            
+            };
+                return rp(bulkRegisterOption);
+        
+        })
+        .then(data => {
+            res.json({ note: 'New node registred with our network seccessfilly.'});
+        });
     });
 
 
 
-//register multupile nodes at ones 
-app.post('/register-nodes-bulk', function(req,res){
-    const allnetworknodes = req.body.allnetworknodes;
-    allnetworknodes.forEach(networknodeUrl=> {
-const nodenotalreadypreast =  Vegawallet.networknodes.indexOf(networknodeUrl) == -1 ;
-const notcurrentnode = Vegawallet.currentNodeUrl !== networknodeUrl;       
-if (nodenotalreadypreast && notcurrentnode) Vegawallet.networknodes.push(networknodeUrl);
+    //recive and register a node with the network
 
-    })
-    res.json({ note: 'bulk registration successfuly.'});
+    app.post('/register-node', function(req, res) {
+        const newNodeUrl = req.body.newNodeUrl;
+        const nodeNotAlreadyPresent = Vegawallet.netWorkNodes.indexOf(newNodeUrl) == -1;
+        const notCurrentNode = Vegawallet.currentNodeUrl !== newNodeUrl;
+        if (nodeNotAlreadyPresent && notCurrentNode) Vegawallet.netWorkNodes.push(newNodeUrl); 
+        res.json({ note: 'New node registered successfully.' });
+    });
+
+
+    //register multupile nodes at ones 
+        app.post('/register-nodes-bulk', function(req,res){
+            const allnetWorkNodes = req.body.allnetWorkNodes;
+            allnetWorkNodes.forEach(netWorkNodeUrl=> {
+            const nodeNoteAlreadyPresent =  Vegawallet.netWorkNodes.indexOf(netWorkNodeUrl) == -1 ;
+            const notCurrentNode = Vegawallet.currentNodeUrl !== netWorkNodeUrl;       
+                if (nodeNoteAlreadyPresent && notCurrentNode) Vegawallet.netWorkNodes.push(netWorkNodeUrl);
+            });
+        res.json({ note: 'bulk registration successfuly.'});
 
 });
 
--- Dev/blockchain.js
+++ Dev/blockchain.js
@@ -1,60 +1,73 @@
 const sha256= require('sha256');
 const currentNodeUrl = process.argv[3];
+const uuid = require('uuid/v1');
+
 
 function Blockchain() {
     this.chain = [];
     this.pendingtransaction= [];
     this.currentNodeUrl=currentNodeUrl;
-    this.networknodes = []; 
-    this.creatnewblock(100,'0','0'); //every blockchain starter with first block ( genisis block)
+    this.netWorkNodes = []; 
+    this.creatNewBlock(100,'0','0'); //every blockchain starter with first block ( genisis block)
 
 }
 
-  Blockchain.prototype.creatnewblock = function(nonce, previousblockhash, hash){
-    const newblock ={
+  Blockchain.prototype.creatNewBlock = function(nonce, previousBlockHash, hash){
+    const newBlock ={
       index:  this.chain.length +1, // creat a new block
       timestamp: Date.now(), // creation date of the block
       transactions: this.pendingtransaction, //put all of the new trransction or all of the pending transactions on the block // ma3neha l new transactions lkol bech yet7atou fil block jdid li 9a3din nasn3ou fih
       nonce: nonce, //
       hash: hash, // hashing mta3 li block hedha
-      previousblockhash: previousblockhash, // hashing mta3 leblock li ba3dou
+      previousBlockHash: previousBlockHash, // hashing mta3 leblock li ba3dou
     };
       this.pendingtransaction = []; //
-      this.chain.push(newblock); // l7al9a lola fi chain ta5o l block hedha
+      this.chain.push(newBlock); // l7al9a lola fi chain ta5o l block hedha
 
-    return newblock;
-   }
+    return newBlock;
+   };
 
   Blockchain.prototype.getlastblock= function(){
     return this.chain[this.chain.length -1]; // fonction trajja3 ekher block
-   }
+   };
 
   Blockchain.prototype.creatnewtransaction = function(amount, sender, recipient){
     const newtransaction = {
-      amount:amount, //9adeh chyab3eth
-      sender:sender, // address mta3 ly chyab3eth
-      recipient:recipient // add mta3 li chye9bel
+      amount: amount, //9adeh chyab3eth
+      sender: sender, // address mta3 ly chyab3eth
+      recipient: recipient,
+      transactionID: uuid().split('_').join('_') // add mta3 li chye9bel
     };
 
-    this.pendingtransaction.push(newtransaction); // pending transactions mazelou mahoumchy validated yet
-      return this.getlastblock()['index'] + 1;
-   }
+    return newtransaction;
+
+    /* this.pendingtransaction.push(newtransaction); // pending transactions mazelou mahoumchy validated yet
+      return this.getlastblock()['index'] + 1; */
+   };
+    Blockchain.prototype.AddTransactionToPendingTransactions = function(transactionObj){
+      this.pendingtransaction= push(pendingtransaction);
+
+
+    }
+
+
+
 
    Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce) {
 	    const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
 	    const hash = sha256(dataAsString);
       return hash;
-   }
-    Blockchain.prototype.proofofwork= function(previousblockhash,currentBlockData){
+   };
+    Blockchain.prototype.proofofwork= function(previousBlockHash,currentBlockData){
       let nonce=0;
-      let hash= this.hashBlock(previousblockhash,currentBlockData,nonce);
+      let hash= this.hashBlock(previousBlockHash, currentBlockData, nonce);
         while(hash.substring(0,4)!== '0000'){
           nonce++;
-          hash = this.hashBlock(previousblockhash,currentBlockData,nonce);
+          hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
           //console.log(hash);
-        }
+        };
       return nonce;
-    }
+    };
    
 
 module.exports = Blockchain;
